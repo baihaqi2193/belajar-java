6821448f28bf412b3c93257ad870fd3c7969de6e
baihaqi2193 <baihaqi2193@gmail.com>
2022-10-30 06:32:19 +0700

    Added Boolean and Char Datatypes file

---
+++ src/CharDataType.java
@@ -0,0 +1,9 @@
+public class CharDataType {
+    public static void main(String[] args) {
+        char h = 'H';
+        char q = 'Q';
+
+        System.out.print(h);
+        System.out.print(q);
+    }
+}
