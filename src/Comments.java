79eddb35696aaf95ab7341e7cc71ff7b129b9337
baihaqi2193 <baihaqi2193@gmail.com>
2022-11-03 10:25:49 +0700

    Final lesson push :)

---
+++ src/Comments.java
@@ -0,0 +1,18 @@
+public class Comments {
+    public static void main(String[] args) {
+        System.out.println(sum(2, 4));
+    }
+
+    /**
+     *  JAVADOCS:
+     *  Menjumlahkan a dengan b
+     *
+     *  @param a nilai a
+     *  @param b nilai b
+     *  @return a + b
+     */
+    static int sum(int a, int b){
+        // Jumlahkan a dengan b
+        return a + b;
+    }
+}
